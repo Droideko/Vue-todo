f0b6561aadf8b7573d2fa13665f66b67d48dda15
Andrii Chapni <andrii.chapni.cr@gmail.com>
2021-02-28 01:34:45 +0300

    VTD-2 ### features -implemented modal for creating todo item

--- src/components/Board/Board.vue
+++ src/components/Board/Board.vue
@@ -4,10 +4,27 @@
     <div class="uk-container uk-container-xlarge uk-margin-top">
       <h1 class="uk-text-center">Todo list</h1>
       <hr class="uk-divider-icon" />
+      <div class="uk-margin-bottom uk-flex uk-flex-right">
+        <button
+          class="uk-button uk-button-primary"
+          @click="show = !show"
+        >
+          Create
+        </button>
+      </div>
       <div v-for='todo in todos' :key='todo.id'>
         <TodoItem :todo='todo'/>
       </div>
+      <div v-if='!isLoaded' class="uk-flex uk-flex-center">
+        <vk-spinner ratio="1.5"></vk-spinner>
+      </div>
+      <h2 v-else-if='!todos.length' class="uk-text-center">There are no Todos to display</h2>
     </div>
+    <ModalCreateItem
+      :show='show'
+      :closeShow='closeShow'
+      :getTodosList='getTodosList'
+    />
   </div>
 </template>
 
@@ -15,24 +32,38 @@
 import HTTP from '../../services/HttpService';
 import Navigation from '../Navigation/Navigation';
 import TodoItem from '../Board/TodoItem/TodoItem';
+import ModalCreateItem from '../Board/ModalCreateItem/ModalCreateItem';
 
 export default {
   name: 'Board',
   components: {
     Navigation,
     TodoItem,
+    ModalCreateItem,
   },
   data() {
     return {
       todos: [],
+      isLoaded: false,
+      show: false,
     };
   },
   mounted() {
-    HTTP.get('todos').then((data) => {
-      this.todos = data;
-    });
+    this.isLoaded = false;
+    this.getTodosList();
   },
-
+  methods: {
+    closeShow() {
+      this.show = false;
+    },
+    async getTodosList() {
+      HTTP.get('todos').then((data) => {
+        this.todos = data;
+      }).finally(() => {
+        this.isLoaded = true;
+      });
+    },
+  },
 };
 </script>
 
---
+++ src/components/Board/TodoItem/TodoItem.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="uk-card uk-card-small uk-card-default uk-card-body uk-margin-bottom">
+    <h3 class="uk-card-title">{{ todo.title }} </h3>
+    <p>{{ todo.description }}</p>
+    <div class="uk-margin uk-grid-small uk-child-width-auto uk-grid">
+      <label>
+        <input
+          class="uk-checkbox"
+          type="checkbox"
+          :checked='todo.isDone'
+        >
+        {{ todo.isDone ? 'Done' : 'In progress' }}
+      </label>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'TodoItem',
+  props: {
+    todo: Object,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
